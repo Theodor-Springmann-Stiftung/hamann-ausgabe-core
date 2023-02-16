9a49d9a41c6ae4539f58cf795cd8d4b48fb671d0
Simon Martens <schnulller@mailbox.org>
2023-02-16 14:34:01 +0100

    Zwischenablage 1. Experiment

---
+++ HaWeb/wwwroot/js/clipboard.js
@@ -0,0 +1,7 @@
+document.addEventListener('copy', function(e){
+    var e = navigator.clipboard.read();
+
+    var text = window.getSelection().toString();
+    e.clipboardData.setData('text/plain', text);
+    e.preventDefault();
+  });
